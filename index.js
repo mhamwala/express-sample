47366f0a565e916e2a60b3bdee8dcb21c7eefa9f
Thomas Leah <thomas@leahfamily.plus.com>
2019-02-22 15:17:38 +0000

    add delay to request response

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    // Generate delay between 0.5 and 1 second.
+    const delay = Math.random() * (1000 - 500) + 500;
+    setTimeout(() => {
+        res.send(`Hello World! Delay: ${delay}ms`);
+    }, delay);
 });
 
 const PORT = process.env.PORT || 8080;
